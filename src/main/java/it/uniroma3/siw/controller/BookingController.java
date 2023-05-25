231161cb67c0dbd229681ab9c4c796103e5494fa
dmytro.kozac <dimak821@gmail.com>
2023-05-25 20:32:07 +0200

    more update

--- src/main/java/it/uniroma3/siw/controller/BookingController.java
+++ src/main/java/it/uniroma3/siw/controller/BookingController.java
@@ -3,24 +3,18 @@ package it.uniroma3.siw.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Comparator;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 
 
-import it.uniroma3.siw.model.Credentials;
 import it.uniroma3.siw.model.Room;
 import it.uniroma3.siw.repository.RoomRepository;
-import it.uniroma3.siw.service.CredentialsService;
 
 
 
@@ -28,8 +22,6 @@ import it.uniroma3.siw.service.CredentialsService;
 @Controller
 public class BookingController {
 
-    @Autowired
-	private CredentialsService credentialsService;
 
     @Autowired
     private RoomRepository roomRepository;
@@ -41,24 +33,27 @@ public class BookingController {
 
 
 @PostMapping("/admin/addRoom")
-public String addRoom(Model model, 
-                    @RequestParam String name, 
-                    @RequestParam String desc, 
-                    @RequestParam Integer capacity, 
-                    @RequestParam Float price, 
-                    @RequestParam String img,
+public String addRoom(
+                    @RequestParam("name") String name, 
+                    @RequestParam("description") String description, 
+                    @RequestParam("capacity") Integer capacity, 
+                    @RequestParam("price") Float price, 
+                    @RequestParam("img") String img,
                     HttpServletRequest request) {
+
+    String referer = request.getHeader("Referer");//uso HttpServletREquest per aggiornare la pagina
+
     Room room = new Room();
     room.setName(name);
-    room.setDesc(desc);
+    room.setDesc(description);
     room.setCapacity(capacity);
     room.setPrice(price);
     room.setImg(img);
     room.setAvailable(true);
     this.roomRepository.save(room);
-
-    String referer = request.getHeader("Referer");//uso HttpServletREquest per aggiornare la pagina
-    return "redirect:"+ referer;
+    
+    return "redirect:" + referer;
+   // return "/admin/roomList";
 
 }
 
@@ -78,52 +73,47 @@ public String addRoom(Model model,
 
 
 @GetMapping("/admin/roomList")
-public String getMovieAdmin(Model model, @RequestParam(required = false) String sort) {
+public String getMovieAdmin(Model model) {
     List<Room> rooms = this.roomRepository.findAll();
 
-    if (sort != null) {
-        if (sort.equals("asc")) {
-            rooms.sort(Comparator.comparing(Room::getName));
-        } else if (sort.equals("desc")) {
-            rooms.sort(Comparator.comparing(Room::getName).reversed());
-        }
-    }
-
     model.addAttribute("room", rooms);
-    return "/admin/RoomListRoomAdd.html";
+    return "/admin/RoomListRoomAdd";
 }
 
 
 
 
 
-    @GetMapping(value = "/index")
-    public String index() {
-        return "index.html";
-    }
+@GetMapping(value = "/index")
+public String index() {
+    return "index";
+}
 
 
-    @GetMapping(value = "/room1Booking")
-    public String getRoom1() {
-        return "room1Booking";
+@GetMapping(value = "/room1Booking")
+public String getRoom1(Model model) {
+    List<Room> rooms = this.roomRepository.findAllByExactName("Camera Singola");
+    model.addAttribute("room", rooms);
 
-    }
-    @GetMapping(value = "/room2Booking")
-    public String getRoom2() {
-        return "room2Booking";
+    return "room1Booking";
 
-    }
+}
+@GetMapping(value = "/room2Booking")
+public String getRoom2() {
+    return "room2Booking";
 
-    @GetMapping(value = "/room3Booking")
-    public String getRoom3() {
-        return "room3Booking";
+}
 
-    }
+@GetMapping(value = "/room3Booking")
+public String getRoom3() {
+    return "room3Booking";
 
-    @GetMapping(value = "/userBookings")
-    public String getUserBookings() {
-        return "userBookings";
-    }
+}
+
+@GetMapping(value = "/userBookings")
+public String getUserBookings() {
+    return "userBookings";
+}
 
   
 
--- src/main/java/it/uniroma3/siw/repository/RoomRepository.java
+++ src/main/java/it/uniroma3/siw/repository/RoomRepository.java
@@ -3,7 +3,9 @@ package it.uniroma3.siw.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import it.uniroma3.siw.model.Room;
 
@@ -14,6 +16,13 @@ public interface RoomRepository extends CrudRepository<Room, Long> {
     List<Room> findByAvailable(boolean available);
 
     Optional<Room> findById(Long id);
+    
+    //find room by name
+
+
+    @Query("SELECT r FROM Room r WHERE r.name = :name")
+    List<Room> findAllByExactName(@Param("name") String name);
+
 
     List<Room> findAll();
     
