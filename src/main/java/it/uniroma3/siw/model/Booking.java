1916c91f2a454011ea2ff61a66b42c9cca1a8c15
dmytro.kozac <dimak821@gmail.com>
2023-05-30 16:10:28 +0200

    fixes

--- src/main/java/it/uniroma3/siw/model/Booking.java
+++ src/main/java/it/uniroma3/siw/model/Booking.java
@@ -27,7 +27,12 @@ public class Booking {
 
     private String bookedByUsername;
 
-    
+    @Column(name = "check_in_date")
+    private LocalDate checkInDate;
+
+    @Column(name = "check_out_date")
+    private LocalDate checkOutDate;
+
 
 
 
